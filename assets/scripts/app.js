5878c70bb3906a9b3e9ae8bf1a929d6322809f00
Stefan <stefankorbakov@gmail.com>
2021-03-09 00:19:36 +0100

    Cleaned up and moved render functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,9 +20,14 @@ class ElementAttribute {
 }
 
 class Component {
-    constructor(renderHookId) {
+    constructor(renderHookId, shouldRender = true) {
         this.hookId = renderHookId;
+        if (shouldRender) {
+            this.render();
+        }
     }
+
+    render() {}
 
     createRootElement(tag, cssClasses, attributes) {
         const rootElement = document.createElement(tag);
@@ -79,8 +84,9 @@ class ShoppingCart extends Component {
 
 class ProductItem extends Component {
     constructor(product, renderHookId) {
-        super(renderHookId);
+        super(renderHookId, false);
         this.product = product;
+        this.render();
     }
 
     addToCart() {
@@ -106,42 +112,55 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-    products = [
-        new Product(
-            'A Pillow',
-            'https://i1.adis.ws/i/dreams/719-00201_main-shot_01_therapur-cool-pillow',
-            'A Soft pillow!',
-            19.99
-        ),
-        new Product(
-            'A Carpet',
-            'https://www.carpetship.com/wp-content/uploads/2019/08/Blue-Tabriz-Rug-Blue-Persian-carpet-for-sale-2x3m-DR406-6870.jpg',
-            'A carpet which you might like - or not!',
-            89.99
-        ),
-    ];
+    products = [];
 
     constructor(renderHookId) {
         super(renderHookId);
+        this.fetchProducts();
+    }
+
+    fetchProducts() {
+        this.products = [
+            new Product(
+                'A Pillow',
+                'https://i1.adis.ws/i/dreams/719-00201_main-shot_01_therapur-cool-pillow',
+                'A Soft pillow!',
+                19.99
+            ),
+            new Product(
+                'A Carpet',
+                'https://www.carpetship.com/wp-content/uploads/2019/08/Blue-Tabriz-Rug-Blue-Persian-carpet-for-sale-2x3m-DR406-6870.jpg',
+                'A carpet which you might like - or not!',
+                89.99
+            ),
+        ];
+        this.renderProducts();
+    }
+
+    renderProducts() {
+        for (const prod of this.products) {
+            new ProductItem(prod, 'prod-list');
+        }
     }
 
     render() {
         const prodList = this.createRootElement('ul', 'product-list', [
             new ElementAttribute('id', 'prod-list'),
         ]);
-        for (const prod of this.products) {
-            const productItem = new ProductItem(prod, 'prod-list');
-            productItem.render();
+        if (this.products && this.products.lenght > 0) {
+            this.renderProducts();
         }
     }
 }
 
-class Shop {
+class Shop extends Component {
+    constructor() {
+        super();
+    }
+
     render() {
         this.cart = new ShoppingCart('app');
-        this.cart.render();
-        const productList = new ProductList('app');
-        productList.render();
+        new ProductList('app');
     }
 }
 
@@ -150,7 +169,6 @@ class App {
 
     static init() {
         const shop = new Shop();
-        shop.render();
         this.cart = shop.cart;
     }
 
